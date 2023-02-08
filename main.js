1ff8ccc0f6f26a620d40bcb829308d823a47c888
aravindhvetri <2002aravinthv@gmail.com>
2023-02-08 16:34:31 +0530

    ubdate

---
+++ main.js
@@ -0,0 +1,18 @@
+//find js-month:
+
+let month=new Date();
+	 let mon=month.getMonth();
+	 
+	 if(mon<=3){
+	     console.log("spring")
+	 }
+	 else if(mon<=7){
+	 console.log("summer")
+	 }
+	 else if(mon<=11){
+	 console.log("winter")
+	 }	 
+	 else{
+	 console.log("none")
+	 }
+	 
